2f8b05765ee790ca2971db051cde5148c131d89e
Tdev01 <teddy.schneiterpro@gmail.com>
2023-02-28 10:15:35 +0100

    update init serveur

--- backend/serveur.js
+++ backend/serveur.js
@@ -1,9 +1,12 @@
 // appelle de la librairie express
 const express = require("express");
+
 // pour aller chercher dans la bibliotèque express quand on appel app
 const app = express();
+
 // definir le port
 const port = 5000;
+
 // lancement du serveur 
 app.listen(port, () => console.log("le serveur à démarré au port " + port ));
 
